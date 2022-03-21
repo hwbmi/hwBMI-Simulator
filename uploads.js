920a781c4694f21ec9d20ba3d04c25ae3dbb39b3
paulchainkang <paul@paul-kang.com>
2022-03-21 12:56:40 +0800

    wip

---
+++ uploads.js
@@ -0,0 +1,85 @@
+var one_rec = 
+{
+  "title":"measure",
+  "model":"DP4800",
+  "sn":"12345678",
+  "total":"1",
+  "packet":[
+    {
+      "index":"1",
+      "id":"T11223344",
+      "nid":"F123456",
+      "tare_weight":"5.0kg",
+      "net_weight":"63.2kg",
+      "height":"175.0cm",
+      "bmi":"20.6",
+      "time":"2020-07-24 13:26:50"
+    }
+  ]
+};
+
+var two_rec = 
+{
+  "title":"measure",
+  "model":"DP4800",
+  "sn":"12345678",
+  "total":"3",
+  "packet":[
+    {
+      "index":"1",
+      "id":"T11223344",
+      "nid":"F123456",
+      "tare_weight":"5.0kg",
+      "net_weight":"63.2kg",
+      "height":"175.0cm",
+      "bmi":"20.6",
+      "time":"2020-07-24 13:26:50"
+    },
+    {
+      "index":"2"
+    },
+    {
+      "index":"3"
+    }
+  ]
+};
+
+var three_rec = 
+{
+  "title":"measure",
+  "model":"DP4800",
+  "sn":"12345678",
+  "total":"3",
+  "packet":[
+    {
+      "index":"1",
+      "id":"T11223344",
+      "nid":"F123456",
+      "tare_weight":"5.0kg",
+      "net_weight":"63.2kg",
+      "height":"175.0cm",
+      "bmi":"20.6",
+      "time":"2020-07-24 13:26:50"
+    },
+    {
+      "index":"2",
+      "id":"T11223344",  
+      "nid":"F123456",
+      "tare_weight":"5.0kg",
+      "net_weight":"63.2kg",
+      "height":"175.0cm",
+      "bmi":"20.6",
+      "time":"2020-07-24 14:26:50"      
+    },
+    {
+      "index":"3",
+      "id":"T11223344",   
+      "nid":"F123456",
+      "tare_weight":"5.0kg",
+      "net_weight":"63.2kg",
+      "height":"175.0cm",
+      "bmi":"20.6",
+      "time":"2020-07-24 15:26:50"      
+    }
+  ]
+};
